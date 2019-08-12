aba0c0724fce765ce76b167628ffae310ccef4ab
Efraim <efraimmarcatto@gmail.com>
2019-08-12 09:34:58 -0300

    Backend e Frontend

---
+++ backend/src/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const DevController = require("./controllers/DevController");
+const DislikeController = require("./controllers/DislikeController");
+const LikeController = require("./controllers/LikeController");
+
+const routes = express.Router();
+
+routes.post("/devs", DevController.store);
+routes.get("/devs", DevController.index);
+routes.post("/devs/:devId/like", LikeController.store);
+routes.post("/devs/:devId/dislike", DislikeController.store);
+
+module.exports = routes;
